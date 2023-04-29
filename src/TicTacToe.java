dfeed699a69fb856b36f92d8a516ff4470cf2965
Александр <hyperpositron@gmail.com>
2023-04-29 13:45:38 +0200

    Основание для игры

---
+++ src/TicTacToe.java
@@ -0,0 +1,8 @@
+public class TicTacToe {
+  private static final char DOT_HUMAN= 'X';//2) Игрок№1 человек
+  private static final char DOT_AI='O'; //3) Игрок№2 Искусственный интеллект (Artificial intelligence)
+    private static int fieldSizeX; //4) размер по оси X
+    private static int fieldSizeY;//5) размер по оси Y
+    private static char[][] field; // 1) нужно поле
+
+}
