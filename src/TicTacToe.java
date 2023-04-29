ea90a2712488aeabda6f5ec7c001760ffc19bf96
Александр <hyperpositron@gmail.com>
2023-04-29 14:00:58 +0200

    Основание и порядок действий для игры

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,8 +1,17 @@
 public class TicTacToe {
-  private static final char DOT_HUMAN= 'X';//2) Игрок№1 человек
-  private static final char DOT_AI='O'; //3) Игрок№2 Искусственный интеллект (Artificial intelligence)
+  private static final char DOT_HUMAN= 'X';//2) Игрок №1 человек
+  private static final char DOT_AI='O'; //3) Игрок №2 Искусственный интеллект (Artificial intelligence)
     private static int fieldSizeX; //4) размер по оси X
     private static int fieldSizeY;//5) размер по оси Y
     private static char[][] field; // 1) нужно поле
+
+  // printField             6) рисование поя
+  // humanTurn              7) нужно действие , ход игрока
+  // isValidCell           11) попал ли человек или AI в ячейку
+  // isValidCell           12) попал ли человек или AI в пустую ячейку
+  // aiTurn                 8) нужно действие , ход AI
+  // checkWin (char DOT)    9) проверка выйграл или нет
+  // isDraw                10) FieldFull есть ли вообще свободные поля(Ничья),
+  // main                  13)
 
 }
