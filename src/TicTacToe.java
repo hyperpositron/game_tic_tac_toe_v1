29c2570d6459e61eae0e14b76422d1c9a75c716f
Александр <hyperpositron@gmail.com>
2023-04-30 12:00:13 +0200

    Создаем метод // printField - рисование поя Создаем ме тод main ,пишем код для создание поля. Код для создание поля, выделяем в метод initField();

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,17 +1,43 @@
 public class TicTacToe {
-  private static final char DOT_HUMAN= 'X';//2) Игрок №1 человек
-  private static final char DOT_AI='O'; //3) Игрок №2 Искусственный интеллект (Artificial intelligence)
-    private static int fieldSizeX; //4) размер по оси X
-    private static int fieldSizeY;//5) размер по оси Y
-    private static char[][] field; // 1) нужно поле
+    private static final char DOT_HUMAN = 'X';    // 2) Игрок №1 человек
+    private static final char DOT_AI = 'O';       // 3) Игрок №2 Искусственный интеллект (Artificial intelligence)
+    private static final char DOT_EMPTY = '_';    // 5.1) Заполнить пустоту
+    private static int fieldSizeX;                // 4) размер по оси X
+    private static int fieldSizeY;                // 5) размер по оси Y
+    private static char[][] field;                // 1) нужно поле
 
-  // printField             6) рисование поя
-  // humanTurn              7) нужно действие , ход игрока
-  // isValidCell           11) попал ли человек или AI в ячейку
-  // isValidCell           12) попал ли человек или AI в пустую ячейку
-  // aiTurn                 8) нужно действие , ход AI
-  // checkWin (char DOT)    9) проверка выйграл или нет
-  // isDraw                10) FieldFull есть ли вообще свободные поля(Ничья),
-  // main                  13)
+    private static void initField() {
+        fieldSizeY = 3;
+        fieldSizeX = 3;
+        field = new char[fieldSizeY][fieldSizeX];
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                field[y][x] = DOT_EMPTY;
+            }
+        }
+    }
+
+    // 6)printField   рисование поя
+    private static void printField() {
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                System.out.print(field[y][x] + "|");
+            }
+            System.out.println();
+        }
+    }
+
+    // humanTurn              7) нужно действие , ход игрока
+    // isValidCell           11) попал ли человек или AI в ячейку
+    // isValidCell           12) попал ли человек или AI в пустую ячейку
+    // aiTurn                 8) нужно действие , ход AI
+    // checkWin (char DOT)    9) проверка выйграл или нет
+    // isDraw                10) FieldFull есть ли вообще свободные поля(Ничья),
+    // main                  13)
+    public static void main(String[] args) {
+        initField();
+        printField();
+    }
+
 
 }
